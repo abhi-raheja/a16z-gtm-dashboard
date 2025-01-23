f74da3d18f5d3714c316c25124eb555bba190a30
Abhi Raheja <abhihereandnow@gmail.com>
2025-01-23 18:49:16 -0500

    Replace Geist fonts with Inter

---
+++ next.config.js
@@ -0,0 +1,22 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  distDir: 'dist',
+  images: {
+    unoptimized: true,
+    domains: [
+      'pbs.twimg.com',
+      'abs.twimg.com',
+      'upload.wikimedia.org',
+      'unavatar.io'
+    ],
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+}
+
+module.exports = nextConfig
